exynos5420/android_device_samsung_chagallwifi

releasetools: make symlinks for sammy wifi mimik and opencl inside build - makes repackrom obsolate. From: diegocr@xda. Thanx!

BoardConfig.mk
@@ -183,6 +183,7 @@ BOARD_HOSTAPD_DRIVER             := NL80211
 BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_bcmdhd
 WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/dhd/parameters/firmware_path"
 WIFI_DRIVER_FW_PATH_STA          := "/system/etc/wifi/bcmdhd_sta.bin"
+TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := device/samsung/chagallwifi/releasetools/ota_from_target_files
 
 # inherit from the proprietary version
 -include vendor/samsung/chagallwifi/BoardConfigVendor.mk

LineageOS/android_device_samsung_chagalllte

BoardConfig.mk
@@ -193,6 +193,7 @@ BOARD_HOSTAPD_PRIVATE_LIB        := lib_driver_cmd_bcmdhd
 WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/dhd/parameters/firmware_path"
 WIFI_DRIVER_FW_PATH_STA          := "/system/etc/wifi/bcmdhd_sta.bin"
 WIFI_DRIVER_FW_PATH_AP           := "/system/etc/wifi/bcmdhd_apsta.bin"
+TARGET_RELEASETOOL_OTA_FROM_TARGET_SCRIPT := device/samsung/chagalllte/releasetools/ota_from_target_files
 
 # inherit from the proprietary version
 -include vendor/samsung/chagalllte/BoardConfigVendor.mk