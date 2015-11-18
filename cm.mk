exynos5420/android_device_samsung_chagallwifi

cm.mk: remove \ from last line.

cm.mk
@@ -10,4 +10,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_MODEL=SM-T800 \
     PRODUCT_NAME=chagallwifi \
     PRODUCT_DEVICE=chagallwifi \
-    TARGET_DEVICE=chagallwifi \
+    TARGET_DEVICE=chagallwifi

LineageOS/android_device_samsung_chagalllte

cm.mk
@@ -13,4 +13,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_MODEL=SM-T805 \
     PRODUCT_NAME=chagalllte \
     PRODUCT_DEVICE=chagalllte \
-    TARGET_DEVICE=chagalllte \
+    TARGET_DEVICE=chagalllte