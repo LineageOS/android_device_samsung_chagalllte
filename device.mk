9d70148dc512df549cb4fabbb3d982c650ae405a
Basil Gello <vasek.gello@gmail.com>
2017-07-03 00:38:53 +0300

    chagalllte: PRODUCT_PROPERTY_OVERRIDES typo fix
    Change-Id: I0d240fef092e7fd6d3450c824b5149362f15b37b

--- device.mk
+++ device.mk
@@ -53,7 +53,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libshim
 
-PRODUCT_PROPERTY_OVERRIDES := \
+PRODUCT_PROPERTY_OVERRIDES += \
     keyguard.no_require_sim=true \
     ro.com.android.dataroaming=true
 
