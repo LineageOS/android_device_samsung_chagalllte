0d9dc0186414bba5f9d1d8d4211adb2b18edc857
Vasyl Gello <vasek.gello@gmail.com>
2018-08-09 21:41:55 +0300

    chagalllte: Adjust lineage.mk
    Fixes the Gerrit changeset for LineageOS official tree
    
    Change-Id: I17432fd3a7b4199ff267da69bd671763b721213b

--- lineage.mk
+++ lineage.mk
@@ -1,13 +1,20 @@
-$(call inherit-product, device/samsung/chagalllte/full_chagalllte.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony.mk)
+
+$(call inherit-product, device/samsung/chagalllte/device.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
-# Inherit more cyanogenmod stuff.
+# Inherit more LineageOS stuff.
 $(call inherit-product, vendor/cm/config/telephony.mk)
 
 PRODUCT_NAME := lineage_chagalllte
 PRODUCT_DEVICE := chagalllte
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_MODEL := SM-T805
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_MODEL=SM-T805 \
