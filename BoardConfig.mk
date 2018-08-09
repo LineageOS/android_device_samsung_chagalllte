exynos5420/android_device_samsung_klimtlte

Switch the cmhw include orders
Needed to include our custom glove mode which enables it for the touchkeys

Change-Id: Id6063ef14d2ac4aca836447a83c5451fc2873e55

BoardConfig.mk
@@ -105,8 +105,8 @@ BOARD_USES_HWC_SERVICES := true
 TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
 
 # Hardware
-BOARD_HARDWARE_CLASS += device/samsung/klimtlte/cmhw
 BOARD_HARDWARE_CLASS += hardware/samsung/cmhw
+BOARD_HARDWARE_CLASS += device/samsung/klimtlte/cmhw
 
 # Keymaster
 BOARD_USES_TRUST_KEYMASTER := true

LineageOS/android_device_samsung_chagalllte

(cherry picked from commit a53e7cec446f3e2a4b6b64a2eb0177fe1217ccaa)

BoardConfig.mk
@@ -100,8 +100,8 @@ BOARD_USES_HWC_SERVICES := true
 TARGET_SPECIFIC_HEADER_PATH := $(LOCAL_PATH)/include
 
 # Hardware
-BOARD_HARDWARE_CLASS += device/samsung/chagalllte/cmhw
 BOARD_HARDWARE_CLASS += hardware/samsung/cmhw
+BOARD_HARDWARE_CLASS += device/samsung/chagalllte/cmhw
 
 # Keymaster
 BOARD_USES_TRUST_KEYMASTER := true