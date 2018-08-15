30110f09f86856321d287201014ef6e22de1df63
Vasyl Gello <vasek.gello@gmail.com>
2018-08-15 23:41:53 +0300

    chagalllte: Import tree scripts from chagallwifi
    Change-Id: Ia5b49afc5403c9e74cb5448b3819bf5778747722

---
+++ extract-files.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+VENDOR=samsung
+DEVICE=chagalllte
+
+BASE=../../../vendor/$VENDOR/$DEVICE/proprietary
+rm -rf $BASE/*
+
+set -e
+
+if [ -z "$STOCK_ROM_DIR" ]; then
+    if [ "$(adb root | grep -c 'adbd')" -eq 1  ]; then
+	echo "adbd root: OK!"
+	adb wait-for-device
+    else
+	echo "/system/bin/gpsd requires root privileges to be extracted"
+	echo "root access is disabled by system setting - enable in settings -> development options"
+	adb wait-for-device
+	exit
+    fi
+fi
+
+for FILE in `egrep -v '(^#|^$)' proprietary-files.txt`; do
+    OLDIFS=$IFS IFS=":" PARSING_ARRAY=($FILE) IFS=$OLDIFS
+    FILE=${PARSING_ARRAY[0]}
+    DEST=${PARSING_ARRAY[1]}
+    if [ -z $DEST ]
+    then
+        DEST=$FILE
+    fi
+    DIR=`dirname $FILE`
+    if [ ! -d $BASE/$DIR ]; then
+        mkdir -p $BASE/$DIR
+    fi
+
+    if [ -z "$STOCK_ROM_DIR" ]; then
+        adb pull /system/$FILE $BASE/$DEST
+    else
+        cp $STOCK_ROM_DIR/$FILE $BASE/$DEST
+    fi
+
+    # if file does not exist try destination
+    if [ "$?" != "0" ]
+    then
+        adb pull /system/$DEST $BASE/$DEST
+    fi
+done
+
+./setup-makefiles.sh
